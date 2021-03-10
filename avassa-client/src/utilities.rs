379528c304c534d29eb4b1334d2f1a58efa137f8
Martin Björklund <mbj@avassa.io>
2021-03-10 10:05:13 +0100

    renamed datacenters to assigned_datacenters

--- avassa-client/src/utilities.rs
+++ avassa-client/src/utilities.rs
@@ -7,7 +7,7 @@ pub mod state {
     use crate::Client;
     /// Returns a list of DCs a tenant can access
     pub async fn datacenters(client: &Client) -> crate::Result<Vec<crate::types::state::DC>> {
-        let dc = client.get_json("/v1/state/datacenters", None).await?;
+        let dc = client.get_json("/v1/state/assigned_datacenters", None).await?;
         Ok(dc)
     }
 }
