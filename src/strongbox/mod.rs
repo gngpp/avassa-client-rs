b1422e925445bcad27b6ebb64d84fc1611f7bc5b
Fredrik Jansson <fj@avassa.io>
2021-06-11 11:51:03 +0200

    Read sbox vault/kv-maps

--- src/strongbox/mod.rs
+++ src/strongbox/mod.rs
@@ -1,44 +1,34 @@
 //!
 //! Strongbox clients
 //!
-// use crate::Client;
-// use log::debug;
-// use serde_json::json;
-// use std::collections::HashMap;
+use crate::{Client, Error};
+use std::collections::HashMap;
 
-// use crate::Result;
+use crate::Result;
 
 /// Certificate API
 pub mod tls;
 
-// pub enum Distribute {
-//     ToAll,
-//     ToNone,
-//     Inherit,
-//     Sites(Vec<String>),
-// }
+const SBOX_VAULTS: &str = "v1/config/strongbox/vault";
 
-/*
-const SBOX_SECRETS: &str = "v1/config/strongbox/secrets";
-
-/// A Strobox secret store can contain one or more `KVMap` key value stores.
-pub struct SecretStore {
+/// A Strobox vault can contain one or more `KVMap` key value stores.
+pub struct Vault {
     client: Client,
-    store_url: String,
+    vault_url: String,
 }
 
-impl SecretStore {
+impl Vault {
     /// List all secret stores
     pub async fn list(client: &Client) -> Result<Vec<String>> {
         let resp: serde_json::Value = client
-            .get_json(&format!("{}/live", SBOX_SECRETS), Some(&[("keys", "")]))
+            .get_json(&format!("{}", SBOX_VAULTS), Some(&[("keys", "")]))
             .await?;
 
         resp.as_array()
             .ok_or(crate::Error::API("Expected an array".into()))?
             .into_iter()
+            .inspect(|s| log::debug!("{:#?}", s))
             .map(|s| {
-                debug!("{:?}", s);
                 s.as_str()
                     .map(str::to_string)
                     .ok_or(crate::Error::API("Expected a name".into()))
@@ -46,77 +36,53 @@ impl SecretStore {
             .collect()
     }
 
-    async fn new(client: Client, name: &str, distribute: bool) -> Result<Self> {
-        let create = json!( {
-            "name": name,
-            "distribute": distribute,
-        });
-
-        client
-            .post_json(&format!("{}/live", SBOX_SECRETS), &create)
-            .await?;
-
-        let store_url = format!("{}/live/{}", SBOX_SECRETS, name);
-
-        Ok(Self { client, store_url })
+    pub(crate) async fn open(client: &Client, vault: &str) -> Result<Self> {
+        let vault_url = format!("{}/{}", SBOX_VAULTS, vault);
+        log::debug!("Opening vault at path: {}", vault_url);
+        // Try to get the sbox vault
+        let _: serde_json::Value = client.get_json(&vault_url, None).await?;
+        Ok(Self {
+            client: client.clone(),
+            vault_url,
+        })
     }
 
-    /// Creates or openes a new secret store that is distributed to all downstream
-    /// data centers
-    pub async fn new_distributed(client: Client, name: &str) -> Result<Self> {
-        Self::new(client, name, true).await
-    }
+    /// Open a KV map
+    pub async fn open_kv_map(&self, name: &str) -> Result<KVMap> {
+        let map_url = format!("{}/kv-maps/{}", self.vault_url, name);
 
-    /// Creates or openes a new secret store that is local to this datacenter
-    pub async fn new_local(client: Client, name: &str) -> Result<Self> {
-        Self::new(client, name, false).await
-    }
+        let json: serde_json::Value = self.client.get_json(&map_url, None).await?;
 
-    /// Open or create a key value map
-    pub async fn kv_map(&self, name: &str) -> Result<KVMap> {
-        let create = json!( {
-            "name": name,
-            "data": {}
-        });
+        let kv = json
+            .as_object()
+            .ok_or(Error::general("expected a JSON object in kv-map"))?;
 
-        let client = self.client.clone();
-        client
-            .post_json(&format!("{}/kv_maps", self.store_url), &create)
-            .await?;
+        let mut cache = HashMap::new();
+        if let Some(data) = kv.get("data").map(|d| d.as_object()).flatten() {
+            for (k, v) in data.into_iter() {
+                cache.insert(
+                    k.clone(),
+                    v.as_str()
+                        .ok_or(Error::general("Expected secret value to be a string"))?
+                        .to_string(),
+                );
+            }
+        }
 
-        let _map_url = format!("{}/kv_maps/{}", self.store_url, name);
-        todo!("get content from sbox");
-        // Ok(KVMap {
-        //     client,
-        //     map_url,
-        //     cache: HashMap::new(),
-        //     dirty: false,
-        // })
+        log::debug!("Successfully loaded {}", name);
+
+        Ok(KVMap { cache })
     }
 }
 
 /// Strongbox key value map
 pub struct KVMap {
-    _client: Client,
-    _map_url: String,
     cache: HashMap<String, String>,
-    _dirty: bool,
 }
 
 impl KVMap {
-    /// Insert key and value
-    pub fn insert(&mut self, key: String, value: String) -> Option<String> {
-        self.cache.insert(key, value)
-    }
-
     /// Try to get a value
     pub fn get(&self, key: &str) -> Option<&String> {
         self.cache.get(key)
     }
-
-    /// Try to remove an entry
-    pub fn remove(&mut self, key: &str) -> Option<String> {
-        self.cache.remove(key)
-    }
 }
-*/
