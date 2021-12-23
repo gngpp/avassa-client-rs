a4d9c4210573bfd6c69098c0686c1df1998c113c
Fredrik Jansson <fj@avassa.io>
2021-12-23 12:28:37 +0100

    Debug for kv-map

--- src/strongbox/mod.rs
+++ src/strongbox/mod.rs
@@ -85,3 +85,9 @@ impl KVMap {
         self.cache.get(key)
     }
 }
+
+impl std::fmt::Debug for KVMap {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.cache.keys()).finish()
+    }
+}
